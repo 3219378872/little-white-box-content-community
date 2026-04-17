fe4fbfee172509309d77c61970b45e869c87fff2
风过无痕 <3219378872@qq.com>
2026-04-17 18:19:23 +0800

    feat(media): ServiceContext 组装 MediaModel + Storage

--- app/media/internal/svc/service_context.go
+++ app/media/internal/svc/service_context.go
@@ -1,13 +1,42 @@
 package svc
 
-import "esx/app/media/internal/config"
+import (
+	"fmt"
+
+	"esx/app/media/internal/config"
+	"esx/app/media/internal/model"
+	"esx/app/media/internal/storage"
+
+	"github.com/zeromicro/go-zero/core/stores/cache"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	Conn       sqlx.SqlConn
+	MediaModel model.MediaModel
+	Storage    storage.ObjectStorage
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.DataSource)
+
+	cacheConf := cache.CacheConf{
+		cache.NodeConf{
+			RedisConf: c.Redis.RedisConf,
+			Weight:    100,
+		},
+	}
+
+	s3Client, err := storage.NewS3Client(c.S3Storage)
+	if err != nil {
+		panic(fmt.Sprintf("media: 对象存储初始化失败: %v", err))
+	}
+
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		Conn:       conn,
+		MediaModel: model.NewMediaModel(conn, cacheConf),
+		Storage:    s3Client,
 	}
 }
